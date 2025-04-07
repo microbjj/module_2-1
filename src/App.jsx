e9548c84fca1fdb59285a2f303b29c938c6ce7b3
microbjj <1705888@mail.ru>
2025-04-07 18:07:40 +0700

    without JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,64 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  // Декларативный стиль
   const [count, setCount] = useState(0);
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {/* Императивный стиль */}
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <p>{new Date().getFullYear()}</p>
-    </>
+  return React.createElement(
+    React.Fragment,
+    null,
+    React.createElement(
+      "div",
+      null,
+      React.createElement(
+        "a",
+        {
+          href: "https://vite.dev",
+          target: "_blank",
+        },
+        React.createElement("img", {
+          src: viteLogo,
+          className: "logo",
+          alt: "Vite logo",
+        })
+      ),
+      React.createElement(
+        "a",
+        {
+          href: "https://react.dev",
+          target: "_blank",
+        },
+        React.createElement("img", {
+          src: reactLogo,
+          className: "logo react",
+          alt: "React logo",
+        })
+      )
+    ),
+    React.createElement("h1", null, "Vite + React"),
+    React.createElement(
+      "div",
+      { className: "card" },
+      React.createElement(
+        "button",
+        { onClick: () => setCount((count) => count + 1) },
+        `count is ${count}`
+      ),
+      React.createElement(
+        "p",
+        null,
+        "Edit ",
+        React.createElement("code", null, "src/App.jsx"),
+        " and save to test HMR"
+      )
+    ),
+    React.createElement(
+      "p",
+      { className: "read-the-docs" },
+      "Click on the Vite and React logos to learn more"
+    ),
+    React.createElement("p", null, new Date().getFullYear())
   );
 }
 
